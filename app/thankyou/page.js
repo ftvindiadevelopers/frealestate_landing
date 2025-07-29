7e568248f9548c8e52d4a9cf2fad6d0e12ebf41f
Sameer Yadav <ftvdeveloper@gmail.com>
2025-07-29 16:47:43 +0530

    Fixed the vercel error as well

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,13 +1,15 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export const Thankyou = () => {
-    return(
-         <div className="w-screen h-screen flex justify-center items-center bg-[#998263]">
-            <div className="md:w-[600px] w-full h-fit flex flex-col justify-center items-center gap-3 p-5">
-                <h1 className="font-serif text-[70px] font-medium text-white">Thankyou</h1>
-                <p className="text-lg font-normal text-center text-gray-100">Thankyou for contacting us, our business executives will be in touch with you soon!</p>
-                <Link href="/" className="text-lg underline text-gray-200">Go back to Homepage</Link>
-            </div>
-        </div>
-    )
+export default function Thankyou() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-[#998263]">
+      <div className="md:w-[600px] w-full h-fit flex flex-col justify-center items-center gap-3 p-5">
+        <h1 className="font-serif text-[70px] font-medium text-white">Thank You</h1>
+        <p className="text-lg font-normal text-center text-gray-100">
+          Thank you for contacting us, our business executives will be in touch with you soon!
+        </p>
+        <Link href="/" className="text-lg underline text-gray-200">Go back to Homepage</Link>
+      </div>
+    </div>
+  );
 }
