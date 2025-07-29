1e77ca494702b7c38f8c998043318f5c4386e015
Sameer Yadav <ftvdeveloper@gmail.com>
2025-07-29 16:41:33 +0530

    Added thankyou page and fixed the error

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
-export default Thankyou => {
+export const Thankyou = () => {
     return(
-        <div className="w-screen h-screen flex justify-center items-center bg-[#998263]">
+         <div className="w-screen h-screen flex justify-center items-center bg-[#998263]">
             <div className="md:w-[600px] w-full h-fit flex flex-col justify-center items-center gap-3 p-5">
                 <h1 className="font-serif text-[70px] font-medium text-white">Thankyou</h1>
                 <p className="text-lg font-normal text-center text-gray-100">Thankyou for contacting us, our business executives will be in touch with you soon!</p>
